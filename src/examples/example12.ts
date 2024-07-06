604b62c1d629243318e09237200eda3908a643d9
ghiscoding <gbeaulac@gmail.com>
2024-07-05 20:47:07 -0400

    feat: upgrade to Slickgrid-Universal v5.3.3

--- src/examples/example12.ts
+++ src/examples/example12.ts
@@ -481,6 +481,9 @@ export default class Example12 {
       },
       // when using the cellMenu, you can change some of the default options and all use some of the callback methods
       enableCellMenu: true,
+      cellMenu: {
+        activateCellOnMenuClick: true, // this is important to know which row index to use for Composite Editor
+      },
       gridMenu: {
         hideToggleDarkModeCommand: false, // disabled command by default
         onCommand: (_, args) => {
